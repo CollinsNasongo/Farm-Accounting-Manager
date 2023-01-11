8ff424f6a9308459ec4edf085eb9a465308526ac
CollinsNasongo <nasongocollins@gmail.com>
2023-01-11 11:58:56 +0300

    Viewing the index page

--- accountant/accountant/settings.py
+++ accountant/accountant/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u9ji4cb70sfo9k)lod5v%u0^ob22!c2bpj+foxs=-b=7b!wzu9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
