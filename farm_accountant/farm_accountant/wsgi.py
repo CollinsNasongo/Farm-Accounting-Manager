38de3848e7364eb1d2033a6ee39e0c00ac25c5fb
CollinsNasongo <nasongocollins@gmail.com>
2023-01-12 22:11:32 +0300

    Restarting new project configuration

--- farm_accountant/farm_accountant/wsgi.py
+++ farm_accountant/farm_accountant/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for accountant project.
+WSGI config for farm_accountant project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accountant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_accountant.settings')
 
 application = get_wsgi_application()
